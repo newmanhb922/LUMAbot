30b59329e636c5ff159c6e53b261e11dd92a0111
Frank Mularcik <mularcikfrank@gmail.com>
2024-03-25 11:50:42 -0400

    Add encoder counting code and encoder Test.

--- src/GeneralControls/Motors/MotorControl.h
+++ src/GeneralControls/Motors/MotorControl.h
@@ -11,8 +11,35 @@
 #define MOTOR_2_DIR         2
 #define MOTOR_3_DIR         22
 #define MOTOR_4_DIR         21
+#define ENCODER_1A          30
+#define ENCODER_1B          26
+#define ENCODER_2A          23
+#define ENCODER_2B          24
+#define ENCODER_3A          27
+#define ENCODER_3B          25
+#define ENCODER_4A          28
+#define ENCODER_4B          29
 
-/// @brief Sets up the i/o pins for the motor direction outputs
+volatile unsigned char last1A;
+volatile unsigned char last1B;
+volatile unsigned char last2A;
+volatile unsigned char last2B;
+volatile unsigned char last3A;
+volatile unsigned char last3B;
+volatile unsigned char last4A;
+volatile unsigned char last4B;
+
+volatile int encoder1Count;
+volatile int encoder2Count;
+volatile int encoder3Count;
+volatile int encoder4Count;
+
+volatile bool count1Changing;
+volatile bool count2Changing;
+volatile bool count3Changing;
+volatile bool count4Changing;
+
+/// @brief Sets up the i/o pins for the motor direction outputs and sets up encoders
 void SetupMotors();
 
 /// @brief Sets the motor direction of a motor
